4e16bb934beba595899ddb03e467f601f66ba418
岡田佳佑 <k-okada@cmind-co.jp>
2021-02-20 22:21:38 +0900

    Heroウィジェットを追加し画像にアニメーションを追加

--- lib/main.dart
+++ lib/main.dart
@@ -2,74 +2,46 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
       title: 'Navigation',
-      home: HomeScreen(),
+      home: MainScreen(),
     ));
 
-class HomeScreen extends StatelessWidget {
+class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Scaffold(
         appBar: AppBar(
-          title: Text('Demo'),
+          title: Text('Main Screen'),
         ),
-        body: Center(
-          child: SelectionButton(),
-        ),
-      );
-}
-
-class SelectionButton extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) => RaisedButton(
-        onPressed: () {
-          _navigateAndDisplaySelection(context);
-        },
-        child: Text('オプションを選択'),
-      );
-
-  Future<void> _navigateAndDisplaySelection(BuildContext context) async {
-    final result = await Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => SelectionScreen()),
-    );
-
-    Scaffold.of(context)
-      ..removeCurrentSnackBar() //すでに表示されているスナックバーがある場合は削除
-      ..showSnackBar(SnackBar(content: Text(result))); //新しいスナックバーを表示
-  }
-}
-
-class SelectionScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          title: Text('選択してください'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.all(8.0),
-                child: RaisedButton(
-                  onPressed: () {
-                    //選択肢１というデータとともに元の画面に戻る処理
-                    Navigator.pop(context, '選択肢1');
-                  },
-                  child: Text('選択肢1'),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(8.0),
-                child: RaisedButton(
-                  onPressed: () {
-                    //選択肢2というデータとともに元の画面に戻る処理
-                    Navigator.pop(context, '選択肢2');
-                  },
-                  child: Text('選択肢2'),
-                ),
-              ),
-            ],
+        body: GestureDetector(
+          onTap: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (_) => DetailScreen()));
+          },
+          //Heroウィジェットで画像クリック時のアニメーションを追加
+          //tagは画面遷移先のHeroと同じにしないと読み込まれない
+          child: Hero(
+            tag: 'imageHero',
+            child: Image.asset(
+              'images/a.jpg',
+              width: 180.0,
+              height: 180.0,
+            ),
           ),
+        ),
+      );
+}
+
+class DetailScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        body: GestureDetector(
+          onTap: () {
+            Navigator.pop(context);
+          },
+          child: Center(
+              child: Hero(
+            tag: 'imageHero',
+            child: Image.asset('images/a.jpg'),
+          )),
         ),
       );
 }
