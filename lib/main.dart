f8021533fab2bf9d433e109ef514648616972f94
岡田佳佑 <k-okada@cmind-co.jp>
2021-03-05 11:18:44 +0900

    scoped_modelを使用

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,75 @@
+import 'dart:developer';
+import 'package:scoped_model/scoped_model.dart';
 import 'package:flutter/material.dart';
 
-import 'my_widget.dart';
-
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp(
+    model: CounterModel(),
+  ));
+}
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  final CounterModel model;
+
+  const MyApp({Key key, @required this.model}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    debugPrint("MyApp is built");
-    return MaterialApp(
-      title: 'InheritedWidgetSample',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return ScopedModel<CounterModel>(
+      model: model,
+      child: MaterialApp(
+        title: 'Scoped Model Demo',
+        home: CounterHome('Scoped Model Demo'),
       ),
-      home: MyWidget(title: 'InheritedWidget'),
+    );
+  }
+}
+
+class CounterModel extends Model {
+  int _counter = 0;
+  int get counter => _counter;
+
+  void increment() {
+    _counter++;
+    notifyListeners();
+  }
+}
+
+class CounterHome extends StatelessWidget {
+  final String title;
+
+  CounterHome(this.title);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('You have pushed the button this many times:'),
+            ScopedModelDescendant<CounterModel>(
+              builder: (context, child, model) {
+                return Text(
+                  model.counter.toString(),
+                  style: Theme.of(context).textTheme.headline4,
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: ScopedModelDescendant<CounterModel>(
+        builder: (context, child, model) {
+          return FloatingActionButton(
+            onPressed: model.increment,
+            tooltip: 'Increment',
+            child: Icon(Icons.add),
+          );
+        },
+      ),
     );
   }
 }
